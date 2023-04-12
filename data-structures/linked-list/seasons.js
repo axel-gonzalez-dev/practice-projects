b75c583a8d80d7b518b7dc3112f21efcdb410e2a
Axel González <axel.gonzalez.dev@gmail.com>
2023-04-12 08:16:56 -0600

    Update comments in season js file

--- data-structures/linked-list/seasons.js
+++ data-structures/linked-list/seasons.js
@@ -1,19 +1,19 @@
 const LinkedList = require('./LinkedList');
 
-// 1
+// 1 - Create new linked list and print it
 const seasons = new LinkedList();
 seasons.printList();
 
-// 2
+// 2 - Add summer and spring to head and print list
 seasons.addToHead('summer');
 seasons.addToHead('spring');
 seasons.printList();
 
-// 3
+// 3 - Add fall and winter to tail and print list
 seasons.addToTail('fall');
 seasons.addToTail('winter');
 seasons.printList();
 
-// 4
+// 4 - Remove head and print list
 seasons.removeHead();
 seasons.printList();
